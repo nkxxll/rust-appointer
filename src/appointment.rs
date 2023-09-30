a58083a1783303cbf45732f73b125c177f57ea6e
nkirschall <niki.nice1203@gmail.com>
2023-10-01 01:43:10 +0200

    Feat: appointment module made

---
+++ src/appointment.rs
@@ -0,0 +1,56 @@
+pub struct Appointment {
+    pub title: String,
+    pub body: String,
+    pub start_time: String,
+    pub end_time: String,
+    pub display: bool,
+    pub verbose: bool,
+}
+
+impl Appointment {
+    pub fn new(
+        title: String,
+        start_time: String,
+        end_time: String,
+        body: Option<String>,
+        display: Option<bool>,
+        verbose: Option<bool>,
+    ) -> Appointment {
+        Appointment {
+            title,
+            body: body.unwrap_or("".to_string()),
+            start_time,
+            end_time,
+            display: display.unwrap_or(false),
+            verbose: verbose.unwrap_or(false),
+        }
+    }
+    pub fn create_appointment(self) {
+        // call a powershell command that creates a appointment over the com api with outlook
+        // make it a format string and paste the right attributes in
+        let mut com = format!(
+            r#"
+            $outlook = New-Object -ComObject Outlook.Application
+            $appointment = $outlook.CreateItem(1)
+            $appointment.Subject = "{}"
+            $appointment.Start = "{}"
+            $appointment.End = "{}"
+            $appointment.Body = "{}"
+            $appointment.ReminderSet = true
+            $appointment.ReminderMinutesBeforeStart = 15
+            $appointment.Save()
+            "#,
+            self.title, self.start_time, self.end_time, self.body
+        );
+        if self.display {
+            com += "\n$appointment.Save()";
+        };
+        // execute the command with powershell
+        let output = std::process::Command::new("powershell")
+            .arg("-Command")
+            .arg(com)
+            .output()
+            .expect("failed to execute process");
+        println!("stdout: {:#?}\nstderr: {:#?}", output.stdout, output.stderr)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod appointment;
+use crate::appointment::Appointment;
+
 fn main() {
-    println!("Hello, world!");
+    let a: Appointment = Appointment::new(
+        String::from("test"),
+        String::from("10.01.2023  02:00"),
+        String::from("10.01.2023  03:00"),
+        Some(String::from("test")),
+        Some(true),
+        Some(true),
+    );
+    a.create_appointment();
 }
